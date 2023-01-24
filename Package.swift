7f6262bfc7368e980fa670dc72017e251fd0df36
Andrew Vitrichenko <andrew@heycharge.com>
2023-01-24 17:47:09 +0200

    Initial commit

--- Package.swift
+++ Package.swift
@@ -9,18 +9,20 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "heycharge_ios_sdk",
-            targets: ["heycharge_ios_sdk"]),
+            targets: ["heycharge_ios_sdk","ios_sdk"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "heycharge_ios_sdk",
-            dependencies: []),
+            dependencies: [.product(name: "SwiftProtobuf", package: "swift-protobuf")]),
+        .binaryTarget(name: "ios_sdk",
+                             path: "artifacts/ios_sdk.xcframework"),
         .testTarget(
             name: "heycharge_ios_sdkTests",
             dependencies: ["heycharge_ios_sdk"]),
--- Sources/heycharge_ios_sdk/heycharge_ios_sdk.swift
+++ Sources/heycharge_ios_sdk/heycharge_ios_sdk.swift
@@ -1,5 +1,4 @@
 public struct heycharge_ios_sdk {
-    public private(set) var text = "Hello, World!"
 
     public init() {
     }
