09c2c5594503e977bf78b63066a1ac06990c9bdb
Andrew Vitrichenko <andrew@heycharge.com>
2023-01-27 20:49:04 +0200

    Testing publishing

--- Package.swift
+++ Package.swift
@@ -5,24 +5,33 @@ import PackageDescription
 
 let package = Package(
     name: "heycharge_ios_sdk",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "heycharge_ios_sdk",
-            targets: ["heycharge_ios_sdk","ios_sdk"]),
+            targets: ["heycharge_ios_sdk","ios-sdk"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.6.1"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "heycharge_ios_sdk",
-            dependencies: [.product(name: "SwiftProtobuf", package: "swift-protobuf")]),
-        .binaryTarget(name: "ios_sdk",
-                             path: "artifacts/ios_sdk.xcframework"),
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+            ]),
+        .binaryTarget(
+            name: "ios-sdk",
+            path: "artifacts/ios-sdk.xcframework"
+        ),
         .testTarget(
             name: "heycharge_ios_sdkTests",
             dependencies: ["heycharge_ios_sdk"]),
